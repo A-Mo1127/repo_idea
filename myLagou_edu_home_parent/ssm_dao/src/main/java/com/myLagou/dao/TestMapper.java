96691a76e2e2efbad523c388fe6b812a407069aa
zhy_git <3528533684@qq.com>
2022-08-30 11:03:01 +0800

    third add GitMapper

--- myLagou_edu_home_parent/ssm_dao/src/main/java/com/myLagou/dao/TestMapper.java
+++ myLagou_edu_home_parent/ssm_dao/src/main/java/com/myLagou/dao/TestMapper.java
@@ -17,4 +17,14 @@ public interface TestMapper {
     //做出修改
      public List<Test> findAllTest2();
 
+     //测试产生冲突
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test8();
+    public void test6();
+    public void test7();
+
 }
